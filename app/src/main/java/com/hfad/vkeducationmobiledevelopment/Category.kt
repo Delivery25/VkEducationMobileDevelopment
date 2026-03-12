662d014fde719994d1a4b82cbf91a3db40e90ebe
Diana <dds.03@mail.ru>
2026-03-12 18:02:12 +0300

    Скопировала код из репозитория

---
+++ app/src/main/java/com/hfad/vkeducationmobiledevelopment/Category.kt
@@ -0,0 +1,5 @@
+package com.hfad.vkeducationmobiledevelopment
+
+enum class Category {
+    FINANCE, TOOLS, APP, GAME
+}
--- app/src/main/java/com/hfad/vkeducationmobiledevelopment/MainActivity.kt
+++ app/src/main/java/com/hfad/vkeducationmobiledevelopment/MainActivity.kt
@@ -5,9 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.safeDrawingPadding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -19,29 +17,23 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             VkEducationMobileDevelopmentTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                AppDetailsScreen(
+                    Modifier
+                        .fillMaxSize()
+                        .safeDrawingPadding()
+                )
                 }
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    VkEducationMobileDevelopmentTheme {
-        Greeting("Android")
-    }
+    AppDetailsScreen(
+        Modifier
+            .fillMaxSize()
+            .safeDrawingPadding()
+    )
 }
